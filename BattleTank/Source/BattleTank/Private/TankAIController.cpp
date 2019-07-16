31cb3c3df6c2ad8368bc8f610a5bd074f3e63838
CookJamie <cookj024@gmail.com>
2019-07-16 10:56:33 +0100

    -Tidy up .h file includes

--- BattleTank/Source/BattleTank/Private/TankAIController.cpp
+++ BattleTank/Source/BattleTank/Private/TankAIController.cpp
@@ -1,6 +1,7 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 
+#include "Tank.h"
 #include "TankAIController.h"
 #include "GameFramework/PlayerController.h"
 #include "BattleTank.h"
--- BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
+++ BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
@@ -1,6 +1,7 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 
+#include "Tank.h"
 #include "TankPlayerController.h"
 #include "BattleTank.h"
 #include "Engine/Classes/GameFramework/PlayerController.h"
