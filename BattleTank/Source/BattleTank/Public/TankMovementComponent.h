a3bc88351716971c80708574b8bdabc27b1c91e7
CookJamie <cookj024@gmail.com>
2019-10-30 13:40:52 +0000

    -Remove individual tank track controls to avoid double input and align with AI controls -Move the position of AI tank 1 which had been hidden and forgotten about -Move RequestDirectMove to be private

--- BattleTank/Source/BattleTank/Public/TankMovementComponent.h
+++ BattleTank/Source/BattleTank/Public/TankMovementComponent.h
@@ -26,10 +26,10 @@ public:
 	UFUNCTION(BlueprintCallable, Category = Input)
 	void IntendTurnRight(float Throw);
 	
-	/** path following: request new velocity */
-	virtual void RequestDirectMove(const FVector& MoveVelocity, bool bForceMaxSpeed) override;
-
 private:
+    /** path following: request new velocity */
+    virtual void RequestDirectMove(const FVector& MoveVelocity, bool bForceMaxSpeed) override;
+
 	UTankTrack* LeftTrack = nullptr;
 	UTankTrack* RightTrack = nullptr;
 };
