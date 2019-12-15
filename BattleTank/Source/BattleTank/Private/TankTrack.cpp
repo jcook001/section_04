34deada3e824c9b941d7aeaf639db6e45f8e62d4
CookJamie <cookj024@gmail.com>
2019-12-15 23:07:28 +0000

    -Tweaked Tank movement values -Make Tank only move when in contact with the ground

--- BattleTank/Source/BattleTank/Private/TankTrack.cpp
+++ BattleTank/Source/BattleTank/Private/TankTrack.cpp
@@ -8,22 +8,46 @@ UTankTrack::UTankTrack()
     PrimaryComponentTick.bCanEverTick = true;
 }
 
+void UTankTrack::BeginPlay()
+{
+    Super::BeginPlay();
+
+    OnComponentHit.AddDynamic(this, &UTankTrack::OnHit);
+}
+
+void UTankTrack::OnHit(UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit)
+{
+    DriveTrack();
+    ApplySidewaysForce();
+    CurrentThrottle = 0;
+}
+
 void UTankTrack::SetThrottle(float Throttle)
 {
-	//TODO clamp throttle value
-	auto ForceApplied = GetForwardVector() * Throttle * TrackMaxDrivingForce;
-	auto ForceLocation = GetComponentLocation();
-	auto TankRoot = Cast<UPrimitiveComponent>(GetOwner()->GetRootComponent());
-	TankRoot->AddForceAtLocation(ForceApplied, ForceLocation);
+    CurrentThrottle = FMath::Clamp<float>(CurrentThrottle + Throttle, -1, 1);
+}
+
+void UTankTrack::DriveTrack()
+{
+    auto ForceApplied = GetForwardVector() * CurrentThrottle * TrackMaxDrivingForce;
+    auto ForceLocation = GetComponentLocation();
+    auto TankRoot = Cast<UPrimitiveComponent>(GetOwner()->GetRootComponent());
+    TankRoot->AddForceAtLocation(ForceApplied, ForceLocation);
 }
 
 void UTankTrack::TickComponent(float DeltaTime, enum ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
 {
     Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+}
 
+void UTankTrack::ApplySidewaysForce()
+{
     auto SlippageSpeed = FVector::DotProduct(GetRightVector(), GetComponentVelocity());
+
     //workout the required acceleration this frame to correct
-    auto CorrectionAcceleration = - SlippageSpeed / DeltaTime * GetRightVector();
+    auto DeltaTime = GetWorld()->GetDeltaSeconds();
+    auto CorrectionAcceleration = -SlippageSpeed / DeltaTime * GetRightVector();
+
     //calculate and apply sideways for (F = m a)
     auto TankRoot = Cast<UStaticMeshComponent>(GetOwner()->GetRootComponent());
     auto CorrectionForce = (TankRoot->GetMass() * CorrectionAcceleration) / 2; //two tracks
